9e6b3b2f59c639d7ef8c7d1097c520df11d21b0d
Sebasvillota20 <juan.villota-c@mail.escuelaing.edu.co>
2020-05-19 07:40:27 -0500

    eliminacion archivos basura

--- PROYECTO_CORRECTO/Proyect_ALSE/app.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication>
-#include "paciente.h"
-
-int main(int argc, char **argv){
-    QCoreApplication a( argc, argv );  // Para que puedan funcionar los timers
-                                       // debe ser una aplicación de Qt.
-    a.exec();                           // Hace posible que la QCoreApplication maneje los threads.
-    return 0;
-}
